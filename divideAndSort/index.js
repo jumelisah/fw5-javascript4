95e0ab7b9b45b782f67e20d97f50a788ddd43db4
juumelisa <jumelisa09@gmail.com>
2022-01-26 08:37:46 +0700

    Tugas 5

--- divideAndSort/index.js
+++ divideAndSort/index.js
@@ -1,58 +1,58 @@
-const prompts = require('prompts')
+const prompts = require('prompts') // Inisialisasi package prompts
 
-const getAngka = (a)=>{
-    let b = parseInt(a)
+const getAngka = (a)=>{ //Function ini untuk memeriksa apakah input dari pengguna adalah angka
+    let b = parseInt(a) //Karena menggunakan type 'text' saat menerima input, maka angka akan diubah menggunakan parseInt. Jika input yang diterima ternyata bukan angka, maka variabel b akan menghasilkan NaN
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
-            if(!isNaN(b)){
+            if(!isNaN(b)){  //Jika variabel b adalah angka, maka program akan me-resolve angka yang diinputkan pengguna
                 resolve(a)
-            }else{
+            }else{ //Jika variabel b bukan angka, maka program akan menampilkan pesan error
                 reject(new Error("Data harus angka"))
             }
         }, 1000)
     })
 }
 
-const divideAndSort = (num)=>{
+const divideAndSort = (num)=>{  //Function ini digunakan untuk membagi dan mengurutkan angka yang telah diinput pengguna
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
-            let hasil = ''
+            let hasil = '' //inisialisasi variabel
             let i = 0
             let objNum = []
             let arrNum = []
             while(i<=num.length){
-                if(num[i]==0 || i==num.length){
-                    arrNum.sort(function(a,b){return a-b})
-                    objNum.push(arrNum)
-                    arrNum = []
+                if(num[i]==0 || i==num.length){ //jika nilai indeks ke-i dari num sama dengan nol atau i telah mencapai panjang num,
+                    arrNum.sort(function(a,b){return a-b}) //maka array akan diurutkan
+                    objNum.push(arrNum) //arrNum akan dimasukkan ke dalam objNum
+                    arrNum = [] //arrNum dikembalikan menjadi variabel kosong
                 }else{
-                    arrNum.push(num[i])
+                    arrNum.push(num[i]) //jika nilai indeks ke-i dari num bukan nol, maka nilai dari indeks ke-i akan dimasukkan ke dalam arrNum 
                 }
                 i++
             }
-            objNum.forEach(element=>{
+            objNum.forEach(element=>{   //semua elemen dalam objNum akan dimasukkan ke dalam hasil
                 for(let a = 0; a<element.length; a++){
                     hasil+=element[a]
                 }
             })
-            resolve(hasil)
+            resolve(hasil) //mengembalikan hasil
         }, 1000)
     })
 }
 
 async function getHasil(){
     try{
-        const res = await prompts({
-            type : 'text',
-            name : 'value',
-            message : 'Masukkan angka'
+        const res = await prompts({ //Dengan menggunakan prompts, program akan meminta input
+            type : 'text',          // type data yang digunakan adalah text
+            name : 'value',         // value nantinya akan diisi dengan input yang diterima program
+            message : 'Masukkan angka' //ini adalah pesan awal yang akan ditampilkan saat meminta input
         })
-        const angka = await getAngka(res.value)
-        const finalRes = await divideAndSort(angka)
-        console.log(finalRes)
+        const angka = await getAngka(res.value) //Inisialisasi variabel angka dengan memanggil function getAngka. Parameter yan digunakan adalah value yang diterima dari input pengguna
+        const finalRes = await divideAndSort(angka) //Setelah mendapatkan nilai angka, lakukan inisialisasi variabel finalRes yang nilainya didapat setelah memanggil function divideAndSort dengan parameternya adalah angka
+        console.log(finalRes) //Setelah selesai, program akan mencetak hasil dari finalRes
     }catch(err){
-        console.log(err.toString())
-        getHasil()
+        console.log(err.toString()) //Setelah memanggil function getAngka ternyata hasilnya error, maka program akan mengeluarkan pesan error
+        getHasil() //Kemudian, program akan meminta pengguna menginputkan data kembali
     }
 }
 
--- polindrom/index.js
+++ polindrom/index.js
@@ -1,24 +1,24 @@
-const prompts = require('prompts');
+const prompts = require('prompts'); //menggunakan package prompts untuk menerima input pengguna
 
-const isPalindrom = (letter)=>{
+const isPalindrom = (letter)=>{ //fungsi untuk mengecek apakah kata adalah palindrom atau bukan
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
-            let i = 0
-            let j = letter.length-1
-            let mid = j/2
-            let result = ''
-            while(i<mid){
-                if(letter[i]===letter[j]){
-                    result = "polindrom"
-                    i++
-                    j--
-                }else{
-                    result = "bukan polindrom"
-                    i = mid
+            let i = 0 //inisialisasi i=0
+            let j = letter.length-1 //inisialisasi j sama dengan banyaknya index dalam array
+            let mid = j/2 //tentukan nilai tengah dengan membagi dua j
+            let result = '' //inisialisasi result
+            while(i<mid){ //jika i kurang dari mid, jalankan perintah
+                if(letter[i]===letter[j]){ //periksa apakah indeks ke-i dan ke-j dari letter sama atau tidak
+                    result = "polindrom" //jika sama, maka result akan diubah menjadi "polindrom"
+                    i++ //nilai i ditambahkan 1
+                    j-- //nilai j dikurangi 1
+                }else{ //jika elemen ke i dan ke-j tidak sama
+                    result = "bukan polindrom" //maka result akan menjadi "bukan polindrom"
+                    i = mid //i diubah menjadi sama dengan mid sehingga tidak akan melakukan pengulangan
                 }
             }
             if(result){
-                resolve(result)
+                resolve(result) //jika result!==NULL, mereturn result
             }else{
                 reject("Data harus STRING")
             }
@@ -26,19 +26,19 @@ const isPalindrom = (letter)=>{
     })
 }
 
-async function inputKata(){
+async function inputKata(){ //dengan menggunakan async function
     try{
         let kata = ''
-        const response = await prompts({
+        const response = await prompts({ //inisialisasi response yang hasilnya didapat setelah menerima input dari pengguna
             type: 'text',
             name: 'meaning',
             message: 'Masukkan kata: '
         });
-        kata = response.meaning
-        const results = await isPalindrom(kata)
-        console.log(results)
+        kata = response.meaning //kata bakan diisi dengan meaning yang diterima dari input pengguna
+        const results = await isPalindrom(kata) //memanggil fungsi isPalindrom dengan parameter kata
+        console.log(results)//mencetak hasil
     }catch(err){
-        console.log(err)
+        console.log(err) //jika terjadi error maka pesan error akan ditampilkan
     }
 }
 inputKata();
